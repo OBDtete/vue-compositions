c56f8b5464c6ff1c58e9aed230f1efa2ad7f5463
omar <ttanagamar@gmail.com>
2022-01-16 21:04:18 +0100

    added Commit & Woooe components

---
+++ src/components/Commit .vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h3>This is the test commit component</h3>
+</template>
+
+<script>
+export default {
+  name: "Commit"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
